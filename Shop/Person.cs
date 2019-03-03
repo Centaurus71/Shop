68898da6e0f19e033742c973b76a6db061cbc91b
Centaurus <Ruslan-2045@mail.ru>
2019-03-03 21:14:00 +0300

    Ruslan made changes

---
+++ Shop/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    enum Profession
+    {
+        Student,
+        Unemployed, //безработный
+        Developer, //разработчик
+        Teacher, //учитель
+        Mechanic, //механик
+        Cook, //повар
+        Sportsman //спортсмен
+    }
+
+    class Person
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Pol { get; set; }
+    }
+}
